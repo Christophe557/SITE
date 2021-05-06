ef5b3c6dafce2e5c898ae85b43f4bc12480d6830
chvellen <christophe.vellen@gmail.com>
2021-05-06 15:36:08 +0200

    initialisation du projet

---
+++ README.md
@@ -0,0 +1,18 @@
+** Site web ecosketch **
+
+Langages utilisés : 
+HTML5, CSS3
+python 3.8
+django 3.1
+
+Base de données : postgresql
+
+
+Version de développement
+adresse locale : http://localhost/
+
+Versions :
+
+V0.0  6/05/2021 - initialisation. Développement - templates : base.html et index.html, css, responsive, et vue "page accueil"
+ 
+
---
+++ projet/projet/urls.py
@@ -0,0 +1,46 @@
+"""projet URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, re_path, include
+from django.conf import settings
+from blog import views
+
+#---- activation en prod ------------
+# from django.views.static import serve
+#-----------------------------------
+
+
+
+urlpatterns = [
+#    path('admin/', admin.site.urls),
+    re_path(r'^$', views.index, name='index'),
+#    re_path(r'^blog/', include('blog.urls', namespace='blog')),
+
+#---- activation en prod pour servir fichiers STATIC ------------
+#---- en dév (serveur local Django) quand DEBUG=False -----------
+#---- et lancer ./manage.py runserver --insecure ----------------
+#   re_path(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
+#-----------------------------------
+
+]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+            re_path(r'^__debug__/', include(debug_toolbar.urls)),
+            ] + urlpatterns
